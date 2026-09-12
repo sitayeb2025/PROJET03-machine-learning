5482bbd56afa8b38801ca2742d32b8d8b0fc54e0
Your Name <your.email@example.com>
2026-09-12 22:36:40 +0200

    download data

--- src/download_data.py
+++ src/download_data.py
@@ -1,6 +1,7 @@
 import kagglehub, shutil, os
 
 os.makedirs("../Data/raw", exist_ok=True)
+os.makedirs("../Data/processed", exist_ok=True)
 
 path = kagglehub.dataset_download("blastchar/telco-customer-churn")
 fichier_csv = [f for f in os.listdir(path) if f.endswith('.csv')][0]
